bb75eeb44a30b8831e983ab3f485ea6f37dc88e3
Jan Engelhardt <jengelh@medozas.de>
2011-11-21 19:47:26 +0100

    build: define project CFLAGS inside main autotools configs
    Good projects should always use these flag combinations -- not just
    when autogen.sh is called, but also when ./configure is run.

--- autogen.sh
+++ autogen.sh
@@ -2,13 +2,6 @@
 
 autoreconf --install --symlink
 
-MYCFLAGS="-g -Wall \
--Wmissing-declarations -Wmissing-prototypes \
--Wnested-externs -Wpointer-arith \
--Wpointer-arith -Wsign-compare -Wchar-subscripts \
--Wstrict-prototypes -Wshadow \
--Wformat-security -Wtype-limits"
-
 libdir() {
 	echo $(cd $1/$(gcc -print-multi-os-directory); pwd)
 }
@@ -17,4 +10,4 @@ args="--prefix=/usr \
 --sysconfdir=/etc \
 --libdir=$(libdir /usr/lib)"
 
-exec ./configure $args CFLAGS="${MYCFLAGS} ${CFLAGS}" "$@"
+exec ./configure $args "$@"
