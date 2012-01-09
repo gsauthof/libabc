fcf472c5bed2cc6147a0b3bba703110c8a52a25f
Kay Sievers <kay.sievers@vrfy.org>
2012-01-09 23:21:27 +0100

    autogen.sh: add CFLAGS and print entire line, so that mouse copy/paste works

--- autogen.sh
+++ autogen.sh
@@ -21,5 +21,5 @@ echo "----------------------------------------------------------------"
 echo "Initialized build system. For a common configuration please run:"
 echo "----------------------------------------------------------------"
 echo
-echo "# ./configure $args"
+echo "./configure CFLAGS='-g -O0' $args"
 echo
